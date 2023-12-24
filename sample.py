fb0507fdf6f67311cf0bd3134005a05a9128a023
gomesuit <9404411+gomesuit@users.noreply.github.com>
2023-12-24 21:18:09 +0900

    add sample

--- sample.py
+++ sample.py
@@ -1,12 +1,12 @@
-import cv2
-import numpy as np
 import insightface
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
 
-app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+app = FaceAnalysis()
 app.prepare(ctx_id=0, det_size=(640, 640))
-img = ins_get_image('t1')
+
+import cv2
+img = cv2.imread('sample.jpg')
+
 faces = app.get(img)
-rimg = app.draw_on(img, faces)
-cv2.imwrite("./t1_output.jpg", rimg)
+
+print(faces)
